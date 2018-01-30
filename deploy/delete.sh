57eb51d21e9d4e9e109f318dc69e2eda71e9955b
dengyc <dengyc2013@gmail.com>
2018-01-30 15:59:25 +0800

    updated

--- deploy/delete.sh
+++ deploy/delete.sh
@@ -1,4 +1,9 @@
 
+kubectl delete -f ./traefik-ingress/1.traefik-rbac.yaml
+kubectl delete -f ./traefik-ingress/2.traefik-deployment.yaml
+kubectl delete -f ./traefik-ingress/3.traefik-ds.yaml
+kubectl delete -f ./traefik-ingress/4.ui.yaml
+
 
 kubectl delete -f ./dashboard/admin-role.yaml
 kubectl delete -f ./dashboard/kubernetes-dashboard.yaml
@@ -6,8 +11,3 @@ kubectl delete -f ./dashboard/kubernetes-dashboard.yaml
 kubectl delete -f ./jenkins/jenkins-deployment.yaml
 kubectl delete -f ./registry/registry-deployment.yaml
 
-
-kubectl delete -f ./traefik-ingress/1.traefik-rbac.yaml
-kubectl delete -f ./traefik-ingress/2.traefik-deployment.yaml
-kubectl delete -f ./traefik-ingress/3.traefik-ds.yaml
-kubectl delete -f ./traefik-ingress/4.ui.yaml
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,8 @@
 
+kubectl create -f ./traefik-ingress/1.traefik-rbac.yaml
+kubectl create -f ./traefik-ingress/2.traefik-deployment.yaml
+kubectl create -f ./traefik-ingress/3.traefik-ds.yaml
+kubectl create -f ./traefik-ingress/4.ui.yaml
 
 kubectl create -f ./dashboard/admin-role.yaml
 kubectl create -f ./dashboard/kubernetes-dashboard.yaml
@@ -7,7 +11,3 @@ kubectl create -f ./jenkins/jenkins-deployment.yaml
 kubectl create -f ./registry/registry-deployment.yaml
 
 
-kubectl create -f ./traefik-ingress/1.traefik-rbac.yaml
-kubectl create -f ./traefik-ingress/2.traefik-deployment.yaml
-kubectl create -f ./traefik-ingress/3.traefik-ds.yaml
-kubectl create -f ./traefik-ingress/4.ui.yaml
